92652a5a682f0fbd422c88d52fab58984535f684
Ingride Lima <55329148+ingrideLima@users.noreply.github.com>
2020-02-07 20:24:41 -0300

    questão 2

---
+++ questão2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio</title>
+</head>
+<body>
+
+ <form action="adicao.php" method = "GET">
+
+    <input type="number" name = "numero1" />
+    <input type="number" name = "numero2" /> 
+
+    <input type="submit" name = "enviar" value = "enviar" /> 
+
+ </form>  
+
+</body>
+</html>
